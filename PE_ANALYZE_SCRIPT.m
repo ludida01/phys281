8de5898713d4a44d3dbbdbd52b06c00a223cdf58
Daniel Opdahl <dopdahl16@gmail.com>
2019-12-02 18:20:18 -0600

    Add files via upload

--- PE_ANALYZE_SCRIPT.m
+++ PE_ANALYZE_SCRIPT.m
@@ -1,5 +1,3 @@
-
-
 TITLE= 'Select the file with the data you want to bring into MATLAB';
 [filename,filepath] = uigetfile('*.*', TITLE); %Prompts the user to select a data file
 full_filename = fullfile( filepath, filename );
@@ -41,6 +39,9 @@ reverse_voltage_V365 = Data(:,24);
 reverse_current_A365 = Data(:,25);
 
 
+
+
+
 firstrun577 = forward_current_A577(1:301);
 secondrun577 = forward_current_A577(302:602);
 thirdrun577 = forward_current_A577(603:903);
@@ -52,7 +53,23 @@ for i = 1:301;
 end
 voltagetouse577 = forward_voltage_V577(1:301);
 
-plot(voltagetouse577, averagecurrent577, 'o');
+figure
+plot(voltagetouse577, averagecurrent577);
+title("Positive Current vs. Voltage (577nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+
+p = polyfit(voltagetouse577, averagecurrent577', 2);
+y1 = polyval(p,voltagetouse577);
+plot(voltagetouse577, y1);
+y2 = polyval(p,linspace(0,60,602));
+plot(linspace(0,60,602),y2);
+q = polyder(p);
+disp("Plateau 577")
+r = roots(q)
+hold off;
+
 
 
 firstrun546 = forward_current_A546(1:301);
@@ -66,7 +83,23 @@ for i = 1:301;
 end
 voltagetouse546 = forward_voltage_V546(1:301);
 
-plot(voltagetouse546, averagecurrent546, 'o');
+figure
+plot(voltagetouse546, averagecurrent546);
+title("Positive Current vs. Voltage (546nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+
+p = polyfit(voltagetouse546, averagecurrent546', 2);
+y1 = polyval(p,voltagetouse546);
+plot(voltagetouse546, y1);
+y2 = polyval(p,linspace(0,60,602));
+plot(linspace(0,60,602),y2);
+q = polyder(p);
+disp("Plateau 546")
+r = roots(q)
+hold off;
+
 
 
 firstrun436 = forward_current_A436(1:301);
@@ -80,7 +113,23 @@ for i = 1:301;
 end
 voltagetouse436 = forward_voltage_V436(1:301);
 
-plot(voltagetouse436, averagecurrent436, 'o');
+figure
+plot(voltagetouse436, averagecurrent436);
+title("Positive Current vs. Voltage (436nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+
+p = polyfit(voltagetouse436, averagecurrent436', 2);
+y1 = polyval(p,voltagetouse436);
+plot(voltagetouse436, y1);
+y2 = polyval(p,linspace(0,60,602));
+plot(linspace(0,60,602),y2);
+q = polyder(p);
+disp("Plateau 436")
+r = roots(q)
+hold off;
+
 
 
 firstrun405 = forward_current_A405(1:301);
@@ -94,7 +143,23 @@ for i = 1:301;
 end
 voltagetouse405 = forward_voltage_V405(1:301);
 
-plot(voltagetouse405, averagecurrent405, 'o');
+figure
+plot(voltagetouse405, averagecurrent405);
+title("Positive Current vs. Voltage (405nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+
+p = polyfit(voltagetouse405, averagecurrent405', 2);
+y1 = polyval(p,voltagetouse405);
+plot(voltagetouse405, y1);
+y2 = polyval(p,linspace(0,60,602));
+plot(linspace(0,60,602),y2);
+q = polyder(p);
+disp("Plateau 405")
+r = roots(q)
+hold off;
+
 
 
 firstrun365 = forward_current_A365(1:301);
@@ -108,4 +173,141 @@ for i = 1:301;
 end
 voltagetouse365 = forward_voltage_V365(1:301);
 
-plot(voltagetouse365, averagecurrent365, 'o');
+figure
+plot(voltagetouse365, averagecurrent365');
+title("Positive Current vs. Voltage (365nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+
+p = polyfit(voltagetouse365, averagecurrent365', 2);
+y1 = polyval(p,voltagetouse365);
+plot(voltagetouse365, y1);
+y2 = polyval(p,linspace(0,60,602));
+plot(linspace(0,60,602),y2);
+q = polyder(p);
+disp("Plateau 365")
+r = roots(q)
+hold off;
+
+
+
+
+
+firstrun_reverse_577 = reverse_current_A577(1:41);
+secondrun_reverse_577 = reverse_current_A577(42:82);
+thirdrun_reverse_577 = reverse_current_A577(83:123);
+
+averagecurrent_reverse_577 =[];
+for i = 1:41;
+    averagecurrent_reverse_577i = (firstrun_reverse_577(i) + secondrun_reverse_577(i) + thirdrun_reverse_577(i));
+    averagecurrent_reverse_577(i) = averagecurrent_reverse_577i;
+end
+voltagetouse_reverse_577 = reverse_voltage_V577(1:41);
+voltagetouse_reverse_577 = -voltagetouse_reverse_577;
+
+figure
+plot(voltagetouse_reverse_577, averagecurrent_reverse_577)
+title("Negative Current vs. Voltage (577nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+p = polyfit(voltagetouse_reverse_577, averagecurrent_reverse_577', 25);
+y1 = polyval(p,voltagetouse_reverse_577);
+plot(voltagetouse_reverse_577, y1);
+
+
+
+firstrun_reverse_546 = reverse_current_A546(1:41);
+secondrun_reverse_546 = reverse_current_A546(42:82);
+thirdrun_reverse_546 = reverse_current_A546(83:123);
+
+averagecurrent_reverse_546 =[];
+for i = 1:41;
+    averagecurrent_reverse_546i = (firstrun_reverse_546(i) + secondrun_reverse_546(i) + thirdrun_reverse_546(i));
+    averagecurrent_reverse_546(i) = averagecurrent_reverse_546i;
+end
+voltagetouse_reverse_546 = reverse_voltage_V546(1:41);
+voltagetouse_reverse_546 = -voltagetouse_reverse_546;
+
+figure
+plot(voltagetouse_reverse_546, averagecurrent_reverse_546)
+title("Negative Current vs. Voltage (546nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+p = polyfit(voltagetouse_reverse_546, averagecurrent_reverse_546', 25);
+y1 = polyval(p,voltagetouse_reverse_546);
+plot(voltagetouse_reverse_546, y1);
+
+
+
+firstrun_reverse_436 = reverse_current_A436(1:41);
+secondrun_reverse_436 = reverse_current_A436(42:82);
+thirdrun_reverse_436 = reverse_current_A436(83:123);
+
+averagecurrent_reverse_436 =[];
+for i = 1:41;
+    averagecurrent_reverse_436i = (firstrun_reverse_436(i) + secondrun_reverse_436(i) + thirdrun_reverse_436(i));
+    averagecurrent_reverse_436(i) = averagecurrent_reverse_436i;
+end
+voltagetouse_reverse_436 = reverse_voltage_V436(1:41);
+voltagetouse_reverse_436 = -voltagetouse_reverse_436;
+
+figure
+plot(voltagetouse_reverse_436, averagecurrent_reverse_436)
+title("Negative Current vs. Voltage (436nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+p = polyfit(voltagetouse_reverse_436, averagecurrent_reverse_436', 25);
+y1 = polyval(p,voltagetouse_reverse_436);
+plot(voltagetouse_reverse_436, y1);
+
+
+
+firstrun_reverse_405 = reverse_current_A405(1:41);
+secondrun_reverse_405 = reverse_current_A405(42:82);
+thirdrun_reverse_405 = reverse_current_A405(83:123);
+
+averagecurrent_reverse_405 =[];
+for i = 1:41;
+    averagecurrent_reverse_405i = (firstrun_reverse_405(i) + secondrun_reverse_405(i) + thirdrun_reverse_405(i));
+    averagecurrent_reverse_405(i) = averagecurrent_reverse_405i;
+end
+voltagetouse_reverse_405 = reverse_voltage_V405(1:41);
+voltagetouse_reverse_405 = -voltagetouse_reverse_405;
+
+figure
+plot(voltagetouse_reverse_405, averagecurrent_reverse_405)
+title("Negative Current vs. Voltage (405nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+p = polyfit(voltagetouse_reverse_405, averagecurrent_reverse_405', 25);
+y1 = polyval(p,voltagetouse_reverse_405);
+plot(voltagetouse_reverse_405, y1);
+
+
+
+firstrun_reverse_365 = reverse_current_A365(1:41);
+secondrun_reverse_365 = reverse_current_A365(42:82);
+thirdrun_reverse_365 = reverse_current_A365(83:123);
+
+averagecurrent_reverse_365 =[];
+for i = 1:41;
+    averagecurrent_reverse_365i = (firstrun_reverse_365(i) + secondrun_reverse_365(i) + thirdrun_reverse_365(i));
+    averagecurrent_reverse_365(i) = averagecurrent_reverse_365i;
+end
+voltagetouse_reverse_365 = reverse_voltage_V365(1:41);
+voltagetouse_reverse_365 = -voltagetouse_reverse_365;
+
+figure
+plot(voltagetouse_reverse_365, averagecurrent_reverse_365)
+title("Negative Current vs. Voltage (365nm Filter)")
+xlabel('Voltage (V)') 
+ylabel('Current (A)')
+hold on;
+p = polyfit(voltagetouse_reverse_365, averagecurrent_reverse_365', 25);
+y1 = polyval(p,voltagetouse_reverse_365);
+plot(voltagetouse_reverse_365, y1);
